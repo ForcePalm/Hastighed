9621acc2aa19cd2a7127e6cfb78444dafa0ad0da
ForcePalm <48830341+ForcePalm@users.noreply.github.com>
2020-09-24 12:40:25 +0200

    V1.1
    UI Ændringer

--- Hastighed Converter/Hastighed Converter/Form1.cs
+++ Hastighed Converter/Hastighed Converter/Form1.cs
@@ -21,8 +21,8 @@ namespace Hastighed_Converter
         {
             //Konvertere Km/t til M/S (Meter I Sekundet)
             double kmh = Convert.ToDouble(textBox2.Text);
-
             double kmsum = kmh * 0.2778;
+
             label4.Text = Convert.ToString(kmsum +" M/S");
         }
 
@@ -30,8 +30,8 @@ namespace Hastighed_Converter
         {
             //Konvertere M/S til Km/t
             double ms = Convert.ToDouble(textBox1.Text);
-
             double mssum = ms * 3.6;
+
             label3.Text = Convert.ToString(mssum +" Km/h");
         }
 
@@ -39,6 +39,11 @@ namespace Hastighed_Converter
         {
             //lukker programmet
             Application.Exit();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
         }
     }
 }
